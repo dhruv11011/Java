fde32c4fba25d37aa3b512db3f0efdb6eecfbb47
DHRUV BHATT <101125371+dhruv11011@users.noreply.github.com>
2024-02-06 20:42:38 +0530

    Add files via upload

---
+++ 01 Greetname1.java
@@ -0,0 +1,7 @@
+
+public class Greetname1{
+    public static void main(String args[]) {
+        
+        System.out.println(" Welocome to java program !" + args[0]);
+    }
+}
---
+++ 02 Greetfivefriend2.java
@@ -0,0 +1,7 @@
+public class Greetfivefriend2 {
+    public static void main(String args[]) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Welcome to java program my friend " +args[i]);
+        } 
+    }
+}
